39e904785c706aa83e1a8ec74b9d2cc93721c910
Paola Garrido <111533069+paolamgarrido@users.noreply.github.com>
2022-11-26 22:27:34 -0600

    Update Reservacion.h

--- Reservacion.h
+++ Reservacion.h
@@ -1,22 +1,44 @@
-#include <iostream>
-using namespace std;
+#ifndef RESERVACION_H
+#define RESERVACION_H
 
-#include <string>
+#include "Persona.h"
+#include "crucero.h"
+
+#include <iostream>
+
+using namespace std;
 
 class Reservacion
 {
-    private:
-        string fecha;
-        int reserva;
-        int costoTotal;
-
     public:
-        Reservacion();
-        void setFecha(string);
-        string getFecha();
-        void setReserva(int);
-        int getReserva();
-        void setCostoTotal(int);
-        int getCostoTotal();
-        string mostrarInfoReservacion(Reservacion);
+        /*Se definen los metodos de los atributos en
+        la clase reservación utilizando getters y setters*/
+        Reservacion(); //Constructor
+        Reservacion(int,string,string,string,Persona,Crucero);
+        void setNumReserva(int);
+        int getNumReserva();
+        void setNomBarco(string);
+        string getNomBarco();
+        void setNomReserva(string);
+        string getNomReserva();
+        void setNomLinea(string);
+        string getNomLinea();
+        //Metodo para mostrar toda la informacion de la reservación.
+        void mostrarInfoReservacion();
+        //Metodos que utilizan composición
+        void guardarNomReserva(Persona nombre);
+        void guardarLineaCrucero(Crucero linea);
+        virtual ~Reservacion();
+
+    private:
+        //Atributos de la clase reservación.
+        int numReserva;
+        string nombreBarco;
+        string nombreReserva;
+        string nombreLinea;
+        //Atributo de las otras clases.
+        Persona nombre;
+        Crucero linea;
 };
+
+#endif // RESERVACION_H
