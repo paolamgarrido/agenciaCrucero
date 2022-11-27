ed483198ee1068eb060347f99ef102aac7c09471
Paola Garrido <111533069+paolamgarrido@users.noreply.github.com>
2022-11-26 22:23:49 -0600

    Update Crucero.h

--- Crucero.h
+++ Crucero.h
@@ -1,30 +1,39 @@
+#ifndef CRUCERO_H
+#define CRUCERO_H
+
 #include <iostream>
 using namespace std;
 
-#include <string>
-
 class Crucero
 {
-    private:
-        string rutaViaje;
-        string habitacion;
-        string comida;
-        string lineaCrucero;
-        string costo;
-
+    /*Se definen los metodos de los atributos en
+    la clase crucero utilizando getters y setters*/
     public:
-        Crucero();
-        void setRutaViaje(string);
-        string getRutaViaje();
-        void setHabitacion(string);
-        string getHabitacion();
-        void setComida(string);
-        string getComida();
+        Crucero();//Constructor
+        Crucero(string,int,string, string,string);
         void setLineaCrucero(string);
         string getLineaCrucero();
-        void setCosto(string);
-        string getCosto();
-        string mostrarInfoCrucero(Crucero);
+        void setCosto(int);
+        int getCosto();
+        void setRutaCrucero(string);
+        string getRutaCrucero();
+        void setFecha(string);
+        string getFecha();
+        void setHabitacion(string);
+        string getHabitacion();
+        //Metodo para mostrar toda la informacion del crucero.
+        void mostrarCrucero();
+        virtual ~Crucero();
+
+    private:
+        //Atributos de la clase crucero.
+        string lineaCrucero;
+        int costo;
+        string rutaCrucero;
+        string fecha;
+        string habitacion;
+
 
 };
 
+#endif // CRUCERO_H
