18abfcd77ade8e7b19ec5c2126010a15ec87c7a0
Paola Garrido <111533069+paolamgarrido@users.noreply.github.com>
2022-11-26 22:16:08 -0600

    Add files via upload

---
+++ CruceroMediterranean.h
@@ -0,0 +1,35 @@
+#ifndef CRUCEROMEDITERRANEAN_H
+#define CRUCEROMEDITERRANEAN_H
+#include "Crucero.h"
+
+#include <iostream>
+using namespace std;
+
+//Metodo herencia
+class CruceroMediterranean : public Crucero
+{
+    public:
+        CruceroMediterranean();//Constructores
+        CruceroMediterranean(string, int, string, string, string, string,string,string);
+        /*Se definen los metodos de los atributos en
+        la clase Crucero Mediterranean utilizando getters y setters*/
+        void setBumperCars(string);
+        string getBumperCars();
+        void setRipcord(string);
+        string getRipcord();
+        void setNorthStar(string);
+        string getNorthStar();
+        //Metodo para mostrar toda la informacion del Crucero Mediterranean.
+        void mostrarMediterranean();
+        virtual ~CruceroMediterranean();
+
+    private:
+        //Atributos de la clase Crucero Mediterranean.
+        string BumperCars;
+        string Ripcord;
+        string NorthStar;
+
+
+};
+
+#endif // CRUCEROMEDITERRANEAN_H
